09a66d2ed6390dd20f7edf8c06cc7f7bf50c28f8
Dewa Bisma <idewagedebisma@gmail.com>
2021-12-03 21:15:30 +0800

    add network programatically

--- components/MetamaskButton/MetamaskButton.jsx
+++ components/MetamaskButton/MetamaskButton.jsx
@@ -4,10 +4,32 @@ import styles from "./MetamaskButton.module.scss";
 const MetamaskButton = () => {
   const { activateBrowserWallet, account } = useEthers();
 
+  const POLYGON_MUMBAI_PARAMS = {
+    chainId: "0x13881",
+    chainName: "Polygon Testnet Mumbai",
+    nativeCurrency: {
+      name: "Matic",
+      symbol: "MATIC",
+      decimals: 18,
+    },
+    rpcUrls: ["https://rpc-mumbai.maticvigil.com/"],
+    blockExplorerUrls: ["https://mumbai.polygonscan.com/"],
+  };
+
+  const addBlockchainNetwork = () => {
+    window.ethereum
+      .request({
+        method: "wallet_addEthereumChain",
+        params: [POLYGON_MUMBAI_PARAMS],
+      })
+      .then((res) => activateBrowserWallet())
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className={`${styles.wrapper} ${!account ? styles.notLogin : ""}`}>
       {!account && (
-        <button className={styles.button} onClick={activateBrowserWallet}>
+        <button className={styles.button} onClick={addBlockchainNetwork}>
           Connect Metamask
         </button>
       )}
