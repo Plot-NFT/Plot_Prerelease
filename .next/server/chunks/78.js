f15541fba7216911656780dd1cb990c2a117b9ee
Dewa Bisma <idewagedebisma@gmail.com>
2021-12-08 19:27:13 +0800

    fix middleware, rebuild, redeploy

--- .next/server/chunks/78.js
+++ .next/server/chunks/78.js
@@ -46,9 +46,11 @@ class MongoDB {
 const protectAPI = (handler)=>{
     return async (req, res)=>{
         let ipAddress = req.socket.remoteAddress;
-        if (ipAddress.substr(0, 7)) {
+        console.log(ipAddress);
+        if (ipAddress.substr(0, 7) === "::ffff:") {
             ipAddress = ipAddress.substr(7);
         }
+        console.log(ipAddress);
         if (ipAddress !== `${process.env.LOCAL_IP}`) {
             return res.status(403).json({
                 success: false,
--- middleware/protectAPI.js
+++ middleware/protectAPI.js
@@ -2,9 +2,13 @@ const protectAPI = (handler) => {
   return async (req, res) => {
     let ipAddress = req.socket.remoteAddress;
 
-    if (ipAddress.substr(0, 7)) {
+    console.log(ipAddress);
+
+    if (ipAddress.substr(0, 7) === "::ffff:") {
       ipAddress = ipAddress.substr(7);
     }
+
+    console.log(ipAddress);
 
     if (ipAddress !== `${process.env.LOCAL_IP}`) {
       return res.status(403).json({ success: false, message: `Forbidden` });
