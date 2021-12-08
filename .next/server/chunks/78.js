62be62cf2eccc9b9d4556ae17925562af26ab414
Dewa Bisma <idewagedebisma@gmail.com>
2021-12-08 20:27:56 +0800

    fix middleware, rebuild, redeploy

--- .next/server/chunks/78.js
+++ .next/server/chunks/78.js
@@ -45,13 +45,10 @@ class MongoDB {
 /* harmony export */ });
 const protectAPI = (handler)=>{
     return async (req, res)=>{
-        let ipAddress = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
-        console.log(ipAddress);
-        if (ipAddress.substr(0, 7) === "::ffff:") {
-            ipAddress = ipAddress.substr(7);
-        }
-        console.log(ipAddress);
-        if (ipAddress !== `${process.env.LOCAL_IP}`) {
+        const referer = req.headers.referer;
+        const host = req.headers.host;
+        const reqKey = `${referer}${host}`;
+        if (reqKey !== `${process.env.REQ_VALIDATION}`) {
             return res.status(403).json({
                 success: false,
                 message: `Forbidden`
